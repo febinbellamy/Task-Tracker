cbbdca9d0e564ef605f03cd7ee19aa9a365e98d5
Febin Bellamy <febinbellamy@gmail.com>
2022-02-25 12:55:54 -0500

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = (props) => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "Feb 5th at 2:30pm",
-        reminder: true,
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
-        id: 2,
-        text: "School Appointment",
-        day: "Feb 23th at 12:30pm",
-        reminder: true,
+      id: 2,
+      text: "School Appointment",
+      day: "Feb 23th at 12:30pm",
+      reminder: true,
     },
     {
-        id: 3,
-        text: "Dentist Appointment",
-        day: "March 9th at 5:30pm",
-        reminder: true,
-    }
-    ])
+      id: 3,
+      text: "Dentist Appointment",
+      day: "March 9th at 5:30pm",
+      reminder: true,
+    },
+  ]);
 
-    const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id !== id))
-    } 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 };
